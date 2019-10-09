e08c37101f4155e19291a3e98b1470a5eefa231e
Oscar García <oscarhero20@gmail.com>
2019-10-08 20:17:36 -0500

    Taxto es una version que me comfirmaron que funciona en sublime pero en dev c++ no, asi que hice otra versión main con scanf

---
+++ TAREAS/2/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main( int argc, char const *argv[])
+{
+	char caracter [1];
+	printf("Ingrese una palabra\n");
+	scanf("%s", &caracter);
+	printf("%s\n", caracter);
+	return 0;
+}
