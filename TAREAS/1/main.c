0357826d2b7c89ae4d0027c4863b3f84f970601f
Oscar García <oscarhero20@gmail.com>
2019-10-08 19:00:05 -0500

    Programa para saber si un numero es par o non

---
+++ TAREAS/1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[1])
+{
+	int x;
+	scanf("%d", &x);
+	if(x%2==0)
+	{
+		printf("Es par\n");
+	}
+	else
+	{
+		printf("Es non\n");
+	}
+	return 0;
+}
+
