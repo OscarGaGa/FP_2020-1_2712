23e334c00ce8d0e3f86109d11fcc399273feb24d
Oscar García <oscarhero20@gmail.com>
2019-10-21 18:10:02 -0500

    Tarea 3 correcta

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
 int main(int argc, char*argv[])
 {
@@ -6,10 +6,10 @@ int main(int argc, char*argv[])
 	x=atof(argv[1]);
 	y=atof(argv[2]);
 	
-	printf("La sema es %g\n", x+y);
-	printf("La resta es %g\n", x-y);
-	printf("La multiplicación es %g\n", x*y);
-	printf("La división es %.2lf\n", x/y);
+	printf("%.2lf\n", x+y);
+	printf("%.2lf\n", x-y);
+	printf("%.2lf\n", x*y);
+	printf("%.2lf\n", x/y);
 	
 	return 0;
 }
