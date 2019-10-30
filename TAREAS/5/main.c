df367354c04074598457a78af8fa5c64b7a933df
Oscar García <oscarhero20@gmail.com>
2019-10-30 07:58:26 -0600

    Tarea 5

--- TAREAS/5/main.c
+++ TAREAS/5/main.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-int valor(char n)
+int valor(char n)//Esta funcion nos serivira para delvolver un numero de un caracter
 {
 	int z;
     switch(n)
@@ -19,8 +19,8 @@ int valor(char n)
 int main(int argc, char* argu[])
 {
 	 int imprimir,
-    coi;
-    for(int i=1; i<argc; i++)
+    coi;//que es lo mismo que int o char
+    for(int i=1; i<argc; i++)//un for que empeice a recorrer la lista en el subindice 1
     {
         coi=argu[i][0]=='J'||argu[i][0]=='Q'||argu[i][0]=='K'||argu[i][0]=='A';
         if(coi==1)
