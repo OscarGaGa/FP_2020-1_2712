ebcdc342f4c45a4d64e81ae56bc8de3658ed9854
Oscar García <oscarhero20@gmail.com>
2019-11-06 20:33:30 -0600

    Tarea 7 corregida, espero me la reciva por favor

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-int main(int argc, char *argv[])//Son los parametros que nos permitirán meter la base y el exponente que queremos aumnetar
+int main (int argc, char*argu[])
 {
-    int b= atoi(argv[1]);//Es para la base
-    int e= atoi(argv[2]);//Es para el exponente
-    int p= pow(b,e);
-    printf("%i\n", p);
-    return 0;
+	int a;//declaramos varbiables
+	int b;
+	int c=1;
+	a=atof(argu[1]);
+	b=atof(argu[2]);
+	for (int i=0; i<b; i ++)//decidimos donde lo vamos a guardar
+	{
+		c=c*a;
+	}
+	printf("%.2d\n", c);
+	return 0;
 }
